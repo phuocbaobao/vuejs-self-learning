2ff3d67efc1ee50fe2b9ef5872e8e29e954330f4
Phuoc Bao <baohp@evolableasia.vn>
2019-12-13 17:11:36 +0700

    multiple choice

---
+++ src/components/Question.vue
@@ -0,0 +1,62 @@
+<template>
+  <md-card
+    class="md-card"
+    v-bind:style="[isChecked ? {'background-color': 'currentColor'} : {'background-color': 'white'}]"
+  >
+    <md-checkbox v-model="isChecked">{{ film }}</md-checkbox>
+  </md-card>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+@Component
+export default class Question extends Vue {
+  @Prop() film!: String;
+  isChecked: boolean = false;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="css" scoped>
+.md-card {
+  display: flex;
+  align-items: center;
+  border-radius: 5px;
+  margin: 5px 10px 5px 10px;
+  width: 100%;
+}
+
+.md-checkbox {
+  display: flex;
+  align-content: space-between;
+  width: inherit;
+  margin: 0;
+  padding: 16px 16px 16px 0;
+}
+
+.md-checkbox >>> label {
+  text-align: left;
+  display: inline-table;
+}
+
+.md-checkbox >>> .md-checkbox-container {
+  border-radius: 100%;
+  border-color: black;
+  align-self: center;
+}
+
+.md-checked >>> .md-checkbox-container {
+  background-color: burlywood;
+}
+
+.md-checked >>> label {
+  color: white;
+}
+
+@media (max-width: 599px) {
+  .md-card {
+    padding-left: 10px !important;
+  }
+}
+</style>
---
+++ src/views/Quiz.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="quiz">
+    <h3>Pick three of your favorite Star Wars films</h3>
+    <div class="md-layout md-gutter question-row">
+      <!-- 25 is 1/4 screen resolution -->
+      <question
+        class="md-layout-item md-small-size-45 md-size-35"
+        v-for="(film, index) in films"
+        v-bind:key="index"
+        v-on:click="onChecked"
+        :film="film"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+import Question from "@/components/Question.vue";
+
+@Component({
+  components: {
+    question: Question
+  },
+  computed: {
+    onChecked: () => console.log("Hello World")
+  }
+})
+export default class Quiz extends Vue {
+  films: String[] = [
+    "Episode I – The Phantom Menace (1999)",
+    "Episode II – Attack of the Clones (2002)",
+    "Episode III – Revenge of the Sith (2005)",
+    "Episode IV – A New Hope (1977)",
+    "Episode V – The Empire Strikes Back (1980)",
+    "Episode VI – Return of the Jedi (1983)",
+    "Episode VII – The Force Awakens (2015)",
+    "Episode VIII – The Last Jedi (2017)",
+    "Episode IX – The Rise of Skywalker (2019)"
+  ];
+}
+</script>
+
+<style lang="scss" scoped>
+h3 {
+  font-size: 25px;
+  line-height: initial;
+}
+
+.question-row {
+  padding: 5px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+</style>
